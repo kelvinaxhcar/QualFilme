3b9ffe2e62993a0dde08b7e619871a004a821c31
Samuel Araag <samuelaraag@iMac-de-Samuel.local>
2022-11-17 22:29:03 -0300

    criado arquivo footer

--- qual-filme/src/app/app.module.ts
+++ qual-filme/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
