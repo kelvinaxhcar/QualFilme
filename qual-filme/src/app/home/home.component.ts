472522087604baa80d55484eb09a0959b9d0577e
Samuel Araag <samuelaraag@iMac-de-Samuel.local>
2022-11-18 23:06:01 -0300

    inicializado implementacao do autocomplete

--- qual-filme/src/app/home/home.component.ts
+++ qual-filme/src/app/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Filmes } from '../models';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
-})
+})  
+
 export class HomeComponent {
 
+  listaDeFilmes: Filmes[] = [
+    {Title: 'Avatar'},
+    {Title: 'Um Estranho no Paraiso'},
+    {Title: 'Doutor Estranho'},
+    {Title: 'Superman'},
+    {Title: 'Matrix'},
+    {Title: 'Alice no Pais Das Maravilhas'},
+    {Title: 'O Tumulo dos Vagalumes'},
+    {Title: 'Seven'},
+    {Title: 'A Espera de Um Milagre'},
+    {Title: 'Forrest Gump'},
+    {Title: 'Mother'},
+    {Title: 'O Silencio dos Inocentes'},
+]
 }
---
+++ qual-filme/src/app/models.ts
@@ -0,0 +1,3 @@
+export class Filmes {
+    Title?: String;
+}
